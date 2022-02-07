0a6d3da1317777d1befbfa726547402bb1cb48ea
Willie Xu <ity@keemail.me>
2022-02-08 04:16:12 +0800

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,16 @@ import { ChakraProvider } from "@chakra-ui/react";
 import Layout from "../components/layouts/main";
 import theme from "../lib/theme";
 import { AnimatePresence } from "framer-motion";
+import { Auth0Provider } from '@auth0/auth0-react'
+
 
 function Website({ Component, pageProps, router }) {
   return (
+        <Auth0Provider
+        domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN}
+        clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID}
+        redirectUri={process.env.NEXT_PUBLIC_URL}
+    >
     <ChakraProvider theme={theme}>
       <Layout router={router}>
         <AnimatePresence exitBeforeEnter initial={true}>
@@ -12,6 +19,7 @@ function Website({ Component, pageProps, router }) {
         </AnimatePresence>
       </Layout>
     </ChakraProvider>
+        </Auth0Provider>
   );
 }
 
