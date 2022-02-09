1b534d0cae6d7baf4f88b8bfc97ca18ffca00779
Willie Xu <ity@keemail.me>
2022-02-09 12:55:11 +0800

    Update form.js

--- components/form.js
+++ components/form.js
@@ -30,8 +30,6 @@ export default function Form({ onSubmit, text, textSet }) {
             <Button colorScheme='teal' p={3} borderWidth="1px" rounded="md">
               Send
             </Button>
-<Flex>
-<>
 <Box mt={10}>
             <Image src={user.picture} alt="logo" width="30px" my={3} mr={5} rounded="full" />
             <span ml={5}>{user.name}</span>
@@ -48,8 +46,6 @@ export default function Form({ onSubmit, text, textSet }) {
               x
          </Button>
 </Box>
-</>
-          </Flex>
 
         ) : (
           <Button
