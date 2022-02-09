dcd2ba4b2349934e297b605ea0df904d42bb5974
Willie Xu <ity@keemail.me>
2022-02-09 12:41:58 +0800

    Update form.js

--- components/form.js
+++ components/form.js
@@ -27,10 +27,12 @@ export default function Form({ onSubmit, text, textSet }) {
 
       <Box mt={3} mb={6}>
         {isAuthenticated ? (
-          <Flex align="center" >
+          <>
             <Button colorScheme='teal' p={3} borderWidth="1px" rounded="md">
               Send
             </Button>
+</>
+<Flex>
 <Box mt={10}>
             <Image src={user.picture} alt="logo" width="30px" my={3} mr={5} rounded="full" />
             <span ml={5}>{user.name}</span>
